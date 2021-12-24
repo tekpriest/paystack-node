303f6e138bc8f7b780a464336ea27a14cbf81b17
Asaju Enitan Michael <asajuenitan@gmail.com>
2021-12-24 12:23:40 +0100

    ✍🏻 UPDATE(paystack): add to main paystack class

--- src/paystack.ts
+++ src/paystack.ts
@@ -1,5 +1,6 @@
 import { Axios } from 'axios';
 import { Charge } from './charge';
+import { Customer } from './customer/customer';
 import { Plan } from './plan';
 import { Transaction } from './transaction';
 
@@ -13,6 +14,7 @@ export class Paystack {
   public charge: Charge;
   public transaction: Transaction;
   public plan: Plan;
+  public customer: Customer
   constructor(public readonly key: string) {
     this.http = new Axios({
       baseURL: 'https://api.paystack.co',
@@ -24,5 +26,6 @@ export class Paystack {
     this.charge = new Charge(this.http);
     this.transaction = new Transaction(this.http);
     this.plan = new Plan(this.http);
+    this.customer = new Customer(this.http)
   }
 }
